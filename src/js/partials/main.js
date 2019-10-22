7a76ee1d01a416e60530f5c56e6db861fda6999d
Andrei Cojocari <webtheory@mail.ru>
2019-10-22 23:40:02 +0300

    fix animation and rander  box shadow

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -9,22 +9,33 @@ var arContactUs = {
                 if (secunds === true) {
                     setTimeout(function(){ 
                         widget_toggle();
-                        secunds  = false;
+                        secunds =  !secunds;
                         console.log('30 secunds',secunds);
-                    }, 30000);
+                    }, 5000);
                 }
-                
+                return secunds;
             }
+
+            var fivesec = true;
+            setTimeout(function(){ 
+                if (fivesec === true && !document.querySelector('.messangers-block').classList.contains('show-messageners-block')) {
+                    widget_toggle();
+                    
+                    fivesec  = false;
+                    console.log('5 secunds open if not click before', fivesec);
+                } 
+            }, 3000);
+
         // Create function for change classes
         function widget_toggle(e) {
             document.querySelector('#arcontactus-widget .messangers-block').classList.toggle('show-messageners-block');
+            document.querySelector('.widget_container').classList.toggle('show_widget');
+
             // document.querySelector('#arcontactus-widget .static').classList.toggle('hide');
             // document.querySelector('#arcontactus-widget .arcontactus-close').classList.toggle('show-messageners-block');`
-            document.querySelector('.widget_container').classList.toggle('show');
 
         }
         document.querySelector("#arcontactus-widget .arcontactus-message-button").addEventListener("click", (e) => {
-
             widget_toggle();
             e.preventDefault();
         });
@@ -46,26 +57,16 @@ var arContactUs = {
 
         });
         // Close by click on document
-        var specifiedElement = document.querySelector(".arcontactus-message");
-        document.addEventListener('click', function(event) {
-          var isClickInside = specifiedElement.contains(event.target);
-          if (!isClickInside) {
-            widget_toggle();
-          }
-        });
+        // var specifiedElement = document.querySelector(".arcontactus-message");
+        // document.addEventListener('click', function(event) {
+        //   var isClickInside = specifiedElement.contains(event.target);
+        //   if (!isClickInside) {
+        //     widget_toggle();
+        //   }
+        // });
 
 
-
-       
-            var fivesec = true;
-            setTimeout(function(){ 
-                if (fivesec === true && !document.querySelector('.messangers-block').classList.contains('show-messageners-block')) {
-                    widget_toggle();
-                    console.log('5 secunds open if not click before', fivesec);
-                    fivesec  = false;
-                } 
-            }, 5000);
-
+           
         document.querySelector('#arcontactus-widget').classList.add('active');
         
     }
