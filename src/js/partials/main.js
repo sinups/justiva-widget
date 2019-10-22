4060e09905b079af82d8db00ef5a797643b0d98d
Andrei Cojocari <webtheory@mail.ru>
2019-10-22 16:55:38 +0300

    update

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,11 +1,23 @@
 "use strict";
 var arContactUs = {
     init: function(){
+        
+        //Function to fix time closed  widget 
+            var secunds = true;
+            function onClickClose() {  
+                if (secunds === true) {
+                    setTimeout(function(){ 
+                        widget_toggle();
+                        secunds  = false;
+                    }, 30000);
+                }
+                
+            }
         // Create function for change classes
         function widget_toggle(e) {
             document.querySelector('#arcontactus-widget .messangers-block').classList.toggle('show-messageners-block');
             // document.querySelector('#arcontactus-widget .static').classList.toggle('hide');
-            // document.querySelector('#arcontactus-widget .arcontactus-close').classList.toggle('show-messageners-block');
+            // document.querySelector('#arcontactus-widget .arcontactus-close').classList.toggle('show-messageners-block');`
             document.querySelector('.widget_container').classList.toggle('show');
 
         }
@@ -17,16 +29,19 @@ var arContactUs = {
         // Close btn X
         document.querySelector("#arcontactus-widget .close_widget").addEventListener("click", (e) => {
             widget_toggle();
+            onClickClose();
             e.preventDefault();
+            
         });
 
        // close by link btn
         document.querySelector("#arcontactus-widget .close-widget").addEventListener("click", (e) => {
             widget_toggle();
+            onClickClose();
             e.preventDefault();
         });
         // Close by click on document
-        var specifiedElement =  document.querySelector(".arcontactus-message ");
+        var specifiedElement = document.querySelector(".arcontactus-message");
         document.addEventListener('click', function(event) {
           var isClickInside = specifiedElement.contains(event.target);
           if (!isClickInside) {
@@ -35,8 +50,13 @@ var arContactUs = {
         });
 
         document.querySelector('#arcontactus-widget').classList.add('active');
+
+        setTimeout(function(){ 
+            widget_toggle();
+        }, 5000);
+
+        
     }
-   
 };
 window.addEventListener('load', function(){
     arContactUs.init();
