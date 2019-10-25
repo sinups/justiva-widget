a98d3129c24a61d42a65317faf5275c9c9bd6892
Andrei Cojocari <webtheory@mail.ru>
2019-10-25 16:36:48 +0300

    update

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,193 @@
+/**
+ * Параметры по умолчанию для нашего приложения
+ * @open Начальное состояние виджета открыт/закрыт.
+ * @openInterval Через сколько должен открываться виджет После загрузки страницы.
+ * @openOnLoadInterval открываем в указанном промежутке.
+ * @userAction Фиксируем 1 Клик который сделал пользователь на виджете.
+ * init() инициализируем функцию при загрузке страницы.
+ */
+
+window.addEventListener('load', function(){
+    jusWidget.open = false;
+    jusWidget.openInterval = 40000;
+    jusWidget.openOnLoadInterval = 15000;
+    jusWidget.userAction = false;
+    jusWidget.init();
+    document.querySelector('#arcontactus-widget').classList.add('active');
+});
+
+let jusWidget = {
+    open: null,
+    countdown: null,
+    openInterval: null,
+    openOnLoadInterval: null,
+    userAction:null,
+    init: function(){
+        //Только для тестирования
+        // console.log('----- Initial State -----');
+        // console.log('open:',jusWidget.open);
+        // console.log('openInterval:',jusWidget.openInterval);
+        // console.log('openOnLoadInterval:', jusWidget.openOnLoadInterval)
+        // console.log('userAction:', jusWidget.userAction)
+        // console.log('----- Initial State -----');
+        // console.log('init time:',new Date().getSeconds())
+        //Только для тестирования
+
+        //открываем в указанном промежутке
+        function loopOpen() {
+            if (jusWidget.open === false) {
+                changeState ();
+                // console.log('every 40sec',new Date().getSeconds())
+            }
+        }
+        var settIntervel = setInterval(loopOpen, jusWidget.openInterval);
+
+        //Через сколько должен открываться виджет После загрузки страницы.
+        setTimeout(function(){ 
+            if (jusWidget.open === false && jusWidget.userAction === false) {
+                changeState ();
+                // console.log('15 sec',new Date().getSeconds())
+            }
+        }, jusWidget.openOnLoadInterval);
+
+        
+        //Меняем состояние нашего приложения
+        function changeState () {
+            jusWidget.open = !jusWidget.open;
+            // console.log('open and chenge state:',jusWidget.open + ' time:',new Date().getSeconds());
+            if (jusWidget.open === true) {
+                document.querySelector('#arcontactus-widget .messangers-block').classList.add('show-messageners-block');
+                document.querySelector('.widget_container').classList.add('show_widget');
+            } else {
+                document.querySelector('#arcontactus-widget .messangers-block').classList.remove('show-messageners-block');
+                document.querySelector('.widget_container').classList.remove('show_widget');
+                
+            }
+            clearTimeout(settIntervel);
+            settIntervel = setInterval(loopOpen, jusWidget.openInterval);
+            //Фиксируем 1 Клик который сделал пользователь на виджете.
+            jusWidget.userAction = true;
+        }
+
+        // закрываем при клике на крестик
+        document.querySelector("#arcontactus-widget .close_widget").addEventListener("click", (e) => {
+            changeState ();
+            //Яндекс.Метрикa: Нажали на “Закрыть виджет”
+            sendGoal('ya_widgetClosePress', 'widgetClosePress')
+        });
+        // закрываем при клике на текст 'не сейчас'
+        document.querySelector("#arcontactus-widget .close-widget").addEventListener("click", (e) => {
+            changeState ();
+            //Яндекс.Метрикa:Нажали в виджете на “Не сейчас”
+            sendGoal('ya_notnowPress', 'notnowPress')
+
+            e.preventDefault();
+        });
+        // При клике на самом виджет
+        document.querySelector("#arcontactus-widget .arcontactus-message-button").addEventListener("click", () => {
+            changeState();
+            //Яндекс.Метрикa:Нажали в виджете на “открыть чат"
+            sendGoal('ya_openPress', 'openPress')
+
+            //Яндекс.Метрикa:Нажали в виджете на “закрыть чат”
+            if (jusWidget.open === false) {
+                sendGoal('ya_closePress', 'closePress')
+            }
+            
+        });
+
+       
+    },
+    injectScripts: function() {
+        // Get HTML head element 
+        var head = document.querySelector('head');  
+        // Create new link Element 
+        var link = document.createElement('link'); 
+        // set the attributes for link element  
+        link.rel = 'stylesheet';  
+        link.type = 'text/css'; 
+        link.href = '/css/style.css';  
+        // Append link element to HTML head 
+        head.appendChild(link);  
+
+          
+        let html = `<div class="arcontactus-message" id="arcontactus-widget" >
+
+    <div class="messangers-block" style="opacity: 0;">
+        <div class="close_widget">
+            <svg width="12" height="13" viewBox="0 0 12 13" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+
+                <g id="Canvas" transform="translate(-829 -1155)">
+                    <g id="Shape">
+                        <use xlink:href="#path0_fill" transform="matrix(0.707107 0.707107 -0.707107 0.707107 835 1151.52)" fill="#FFFFFF" />
+                    </g>
+                </g>
+                <defs>
+                    <path id="path0_fill" fill-rule="evenodd" d="M 14 6L 8 6L 8 0L 6 0L 6 6L 0 6L 0 8L 6 8L 6 14L 8 14L 8 8L 14 8L 14 6Z" />
+                </defs>
+            </svg>
+
+        </div>
+        <div class="widget__box">
+
+            <div class="justiva__widge__avatar">
+                <img src="img/Bitmap.jpg" alt="">
+                        </div>
+
+
+                <div class="widget__dialog">
+                    <div class="widget__breadcrumbs">
+                        <ul>
+                            <li class="consultant__name">Виктория</li>
+                            <li class="consultant__status">Дежурный юрист из Юстива</li>
+                        </ul>
+                    </div>
+                    <p>
+                        Здравствуйте. <br>
+                        Возможно вам нужна помощь <br>
+                        юриста? Давайте я вам помогу
+                    </p>
+                </div>
+
+            </div>
+            <div class="widget__links">
+                <a href="https://justiva.ru/how-to/online-consultation" target="_blank" class="juw-how-works">Как это работает?</a>
+                <a href="javascript:void(0);" class="close-widget">Не сейчас</a>
+                <a href="https://justiva.ru/questions/ask" target="_blank" class="juw-help-jurist">Да, нужна помощь юриста</a>
+            </div>
+        </div>
+
+        <div class="arcontactus-message-button">
+
+            <div class="static">
+                <!-- <img src="img/mes.png" alt=""> -->
+                <svg class="widget_icon" width="26px" height="26px">
+                    <use xlink:href="img/sprite-svg.svg#mess-icon"></use>
+                </svg>
+            </div>
+
+            <!-- <div class="arcontactus-close">
+                        <img src="./img/close.svg" alt="Close">
+                    </div> -->
+            <div class="pulsation"></div>
+
+        </div>
+
+    </div>
+`;
+        let widgetContainer = document.createElement('div');
+        widgetContainer.classList.add('widget_container');
+        widgetContainer.innerHTML = html;
+
+        document.querySelector('body').appendChild(widgetContainer);
+    },
+
+};
+jusWidget.injectScripts();
+
+
+
+;
 //Цели для Яндекс.Метрики
 let yandexTargets = {
     ya_widgetClosePress:true,
@@ -98,106 +288,4 @@ document.querySelector("#arcontactus-widget .justiva__widge__avatar").addEventLi
 document.querySelector("#arcontactus-widget .juw-how-works").addEventListener("mouseover", () => {
     sendGoal('ya_howitworksOver', 'howitworksOver')
 });
-;
-
-/**
- * Параметры по умолчанию для нашего приложения
- * @open Начальное состояние виджета открыт/закрыт.
- * @openInterval Через сколько должен открываться виджет После загрузки страницы.
- * @openOnLoadInterval открываем в указанном промежутке.
- * @userAction Фиксируем 1 Клик который сделал пользователь на виджете.
- * init() инициализируем функцию при загрузке страницы.
- */
-window.addEventListener('load', function(){
-    jusWidget.open = false;
-    jusWidget.openInterval = 40000;
-    jusWidget.openOnLoadInterval = 15000;
-    jusWidget.userAction = false;
-    jusWidget.init();
-});
-
-let jusWidget = {
-    open: null,
-    countdown: null,
-    openInterval: null,
-    openOnLoadInterval: null,
-    userAction:null,
-    init: function(){
-        //Только для тестирования
-        console.log('----- Initial State -----');
-        console.log('open:',jusWidget.open);
-        console.log('openInterval:',jusWidget.openInterval);
-        console.log('openOnLoadInterval:', jusWidget.openOnLoadInterval)
-        console.log('userAction:', jusWidget.userAction)
-        console.log('----- Initial State -----');
-        console.log('init time:',new Date().getSeconds())
-        //Только для тестирования
-
-        //открываем в указанном промежутке
-        function loopOpen() {
-            if (jusWidget.open === false) {
-                changeState ();
-                console.log('every 40sec',new Date().getSeconds())
-            }
-        }
-        var settIntervel = setInterval(loopOpen, jusWidget.openInterval);
-
-        //Через сколько должен открываться виджет После загрузки страницы.
-        setTimeout(function(){ 
-            if (jusWidget.open === false && jusWidget.userAction === false) {
-                changeState ();
-                console.log('15 sec',new Date().getSeconds())
-            }
-        }, jusWidget.openOnLoadInterval);
-
-        
-        //Меняем состояние нашего приложения
-        function changeState () {
-            jusWidget.open = !jusWidget.open;
-            console.log('open and chenge state:',jusWidget.open + ' time:',new Date().getSeconds());
-            if (jusWidget.open === true) {
-                document.querySelector('#arcontactus-widget .messangers-block').classList.add('show-messageners-block');
-                document.querySelector('.widget_container').classList.add('show_widget');
-            } else {
-                document.querySelector('#arcontactus-widget .messangers-block').classList.remove('show-messageners-block');
-                document.querySelector('.widget_container').classList.remove('show_widget');
-                
-            }
-            clearTimeout(settIntervel);
-            settIntervel = setInterval(loopOpen, jusWidget.openInterval);
-            //Фиксируем 1 Клик который сделал пользователь на виджете.
-            jusWidget.userAction = true;
-        }
-
-        // закрываем при клике на крестик
-        document.querySelector("#arcontactus-widget .close_widget").addEventListener("click", (e) => {
-            changeState ();
-            //Яндекс.Метрикa: Нажали на “Закрыть виджет”
-            sendGoal('ya_widgetClosePress', 'widgetClosePress')
-        });
-        // закрываем при клике на текст 'не сейчас'
-        document.querySelector("#arcontactus-widget .close-widget").addEventListener("click", (e) => {
-            changeState ();
-            //Яндекс.Метрикa:Нажали в виджете на “Не сейчас”
-            sendGoal('ya_notnowPress', 'notnowPress')
-
-            e.preventDefault();
-        });
-        // При клике на самом виджет
-        document.querySelector("#arcontactus-widget .arcontactus-message-button").addEventListener("click", () => {
-            changeState();
-            //Яндекс.Метрикa:Нажали в виджете на “открыть чат"
-            sendGoal('ya_openPress', 'openPress')
-
-            //Яндекс.Метрикa:Нажали в виджете на “закрыть чат”
-            if (jusWidget.open === false) {
-                sendGoal('ya_closePress', 'closePress')
-            }
-            
-        });
-
-        document.querySelector('#arcontactus-widget').classList.add('active');
-    }
-};
-
 ;
